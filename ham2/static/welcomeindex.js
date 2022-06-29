84b00e5d8fc4346e9a8d2117f22a03764920cb9c
macbook <macbook@macbooks-MacBook-Pro.local>
2022-06-30 00:44:41 +0100

    adding starting project

---
+++ ham2/static/welcomeindex.js
@@ -0,0 +1,8 @@
+$(function callback() {
+    setTimeout(function () {
+        $('.fly-in-text').removeClass('hidden');
+    }, 500);
+});
+/*$(window).load(
+window.location.href = "H.A.M..html"
+)*/
