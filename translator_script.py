93acc51e97f11f19ac68a9a9424c53ef5f46713c
macbook <macbook@macbooks-MacBook-Pro.local>
2022-06-30 00:19:15 +0100

    adding starting project

---
+++ translator_script.py
@@ -0,0 +1,14 @@
+import re
+from translate import Translator
+
+translator = Translator(to_lang="ja")
+
+try:
+    with open('./test.txt', mode='r') as my_file:
+        text = my_file.read()
+        translation = translator.translate(text)
+        with open('test-ja.txt', mode='w') as my_file2:
+            my_file2.write(translation)
+        print(translation)
+except FileNotFoundError as e:
+    print('check your file path silly')
